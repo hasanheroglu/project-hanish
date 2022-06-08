a6f71a59c8f28dcc4ca606e2d12de7683b54a898
Hasan Eroglu <hasanheroglu@gmail.com>
2022-06-08 21:35:30 +0200

    Added message producer and ksql stream script.

---
+++ hasan/message_producer.py
@@ -0,0 +1,38 @@
+from kafka3 import KafkaProducer
+import json
+
+class MessageProducer:
+    broker = ""
+    topic = ""
+    producer = None
+
+    def __init__(self, broker, topic):
+        self.broker = broker
+        self.topic = topic
+        self.producer = KafkaProducer(bootstrap_servers=self.broker,
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        acks='all',
+        retries = 3)
+
+
+    def send_msg(self, msg):
+        print("sending message...")
+        try:
+            future = self.producer.send(self.topic,msg)
+            self.producer.flush()
+            future.get(timeout=60)
+            print("message sent successfully...")
+            return {'status_code':200, 'error':None}
+        except Exception as ex:
+            return ex
+
+
+broker = 'localhost:9092'
+topic = 'project-hanish'
+
+message_producer = MessageProducer(broker,topic)
+
+input_file = open("events.json", 'r', encoding='utf-8')
+
+for line in input_file:
+    resp = message_producer.send_msg(json.loads(line))
