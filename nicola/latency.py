4f3b173ca0d566dcdd0b1bc0ee05bd23d2b97a96
Nicola Crivellin <nicola.crivellin98@gmail.com>
2022-07-09 12:17:35 +0200

    Add event count limit to nicola/latency.py

--- nicola/latency.py
+++ nicola/latency.py
@@ -8,13 +8,16 @@ from kafka3 import KafkaConsumer
 
 def main():
     parser = argparse.ArgumentParser(description='Prints latencies in milliseconds.')
+    parser.add_argument('-n', '--num-events', type=int, help='Exit after consuming N events.')
     parser.add_argument('--server', default='localhost:9092')
     parser.add_argument('--topic')
     args = parser.parse_args()
 
     consumer = KafkaConsumer(args.topic, bootstrap_servers=args.server)
     try:
-        for msg in consumer:
+        for i, msg in enumerate(consumer):
+            if args.num_events and i >= args.num_events:
+                break
             current_time = time.time_ns() / 1000000  # Convert ns to ms
             latency = current_time - msg.timestamp
             print(latency)
